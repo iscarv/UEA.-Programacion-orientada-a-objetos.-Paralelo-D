0c15ea08c24ea50b47a22928635025eefe78884b
iscarv <im.ortizs@uea.edu.ec>
2024-07-21 15:07:06 -0500

    Tarea de POO

--- Semana 8. Organizacion de un proyecto orientado a objetos/dashboard.py
+++ Semana 8. Organizacion de un proyecto orientado a objetos/dashboard.py
@@ -2,7 +2,7 @@
 import os
 
 def mostrar_codigo(ruta_script):
-    # Asegúrate de que la ruta al script es absoluta
+    # Asegura que la ruta al script es absoluta
     ruta_script_absoluta = os.path.abspath(ruta_script)
     try:
         with open(ruta_script_absoluta, 'r') as archivo:
